fa3de00a8391359d8dd2c4a495a96cd0335e6125
vic3008 <162638777+vic3008@users.noreply.github.com>
2024-10-08 00:20:04 +0100

    Cal

---
+++ Js Cal Ass/index.js
@@ -0,0 +1,60 @@
+alert("A simple arithemetic showing diffrent ways of calculating");
+
+document.write("<br>");
+document.write("<br>");
+
+document.write("Hello Akorede");
+document.write("<br>");
+document.write("<br>");
+
+// ADDITION
+
+const rice = 15;
+let beans = 20;
+let goods = rice + beans;
+document.write("The packages are  " +  goods);
+document.write("<br>");
+document.write("<br>");
+
+// For Subraction
+
+const green = 50;
+let yellow = 30;
+let color = green - yellow;
+document.write("The packages are  " +  color);
+document.write("<br>");
+document.write("<br>");
+
+// Multiplication
+
+var boys = 20;
+const girls = 15;
+let students = boys * girls;
+document.write("The total number of the Students are  " + students);
+document.write("<br>");
+document.write("<br>");
+
+// Division
+
+var mangos = 10;
+let orange = 5;
+var fruits = orange / mangos;
+document.write("The total number is  " + fruits) 
+document.write("<br>");
+document.write("<br>");
+
+// Raise to Power
+var english = 50
+let maths = 40
+var remark = english ** maths
+document.write("The results is  " + remark)
+document.write("<br>");
+document.write("<br>");
+
+// Percentage
+var yoruba = 60
+let igbo = 50
+var Percentage = yoruba % igbo
+document.write("the outcome  " + Percentage);
+
+
